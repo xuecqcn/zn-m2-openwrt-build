274346c5761f423830370abcc44c8addb9cb8bec
xuecqcn <33610948+xuecqcn@users.noreply.github.com>
2025-01-10 10:04:32 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -12,3 +12,17 @@
 
 # Uncomment a feed source
 #echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+
+#以下是自行添加
+#添加pushbot
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+
+# 添加mosdns drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 添加adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
